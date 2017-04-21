6dcbc82a7e658d2b6dd59dda5374737aa9de12b6
Jaime Hall <Jaime Hall@Jaime-PC.Speedport_W723_V_Typ_A_1_01_017>
2017-04-21 17:46:23 +0200

    Fenster wird erstellt + Kommentierung.

--- IT-Adventures/src/com/it/main/Game.java
+++ IT-Adventures/src/com/it/main/Game.java
@@ -1,11 +1,19 @@
 package com.it.main;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/*
+ * Klasse erweitert Canvas um ein Fenster zum anzeigen des Spiels zu erstellen.
+ * Klasse implementiert Runnable um die Methode run zu Nutzen, welche beim starten eines neuen Threads aufgerufen wird.
+ */
 
 public class Game extends Canvas implements Runnable{
 
-	public static final int WIDTH=1920;
-	public static final int HIEGHT=WIDTH/16*9;
+	public static final int WIDTH=1920;//Klassenkonstante zum Festlegen der Breite des Fensters.
+	public static final int HIEGHT=WIDTH/16*9;//Klassenkonstante welche mithilfe der Breite die Höhe des Fensters errechnet.
 	/**
 	 * 
 	 */
@@ -18,7 +26,14 @@ public class Game extends Canvas implements Runnable{
 	}
 
 	public static void main(String[] args){
-		Game game = new Game();
+		Game game = new Game();//erstellt eine neue Intanz der Game Klasse.
+		JFrame frame = new JFrame();//ein neuer JFrame wird erstellt.
+		
+		game.setMinimumSize(new Dimension(WIDTH,HEIGHT));//setzt die Mindestgröße des Spielfensters.
+		game.setPreferredSize(new Dimension(WIDTH,HEIGHT));//setzt die bevorzugte Größe des Spielfensters.
+		game.setMaximumSize(new Dimension(WIDTH,HEIGHT));//setzt die maximale Größe des Spielfensters.
+		
+		frame.add(game);//Das Spiel wird dem JFrame hinzugefügt um Fensterpreferenzen zu übernehmen.
 		
 	}
 	
