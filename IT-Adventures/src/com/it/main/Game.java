e3492f1d259b3ee9a65d86cec1ad493a3f9c48f7
Jaime Hall <Jaime Hall@Jaime-PC.Speedport_W723_V_Typ_A_1_01_017>
2017-04-21 15:32:49 +0200

    First Game Class changes.

--- IT-Adventures/src/com/it/main/Game.java
+++ IT-Adventures/src/com/it/main/Game.java
@@ -1,5 +1,20 @@
 package com.it.main;
 
-public class Game {
+import java.awt.Canvas;
 
+public class Game extends Canvas implements Runnable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
