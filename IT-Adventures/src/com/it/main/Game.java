b65bf188094c6bb6a8b2ea33e3571b9e2c981097
Vince <Vince@192.168.1.3>
2017-04-22 20:38:49 +0200

    Displaying an image.

--- IT-Adventures/src/com/it/main/Game.java
+++ IT-Adventures/src/com/it/main/Game.java
@@ -2,7 +2,13 @@ package com.it.main;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 /*
@@ -14,6 +20,7 @@ public class Game extends Canvas implements Runnable{
 
 	public static final int WIDTH=1920;//Klassenkonstante zum Festlegen der Breite des Fensters.
 	public static final int HEIGHT=WIDTH/16*9;//Klassenkonstante welche mithilfe der Breite die Höhe des Fensters errechnet.
+	
 	/**
 	 * 
 	 */
@@ -21,10 +28,66 @@ public class Game extends Canvas implements Runnable{
 	
 	private Thread thread;//Variable zum Speichern des Threads.
 	private boolean running;//Boolsche Variable die bestimmt ob das Spiel am laufen ist.
+	
+	private BufferedImage background;
 
 	@Override
-	public void run() {//SFKJHSDIF
-		// TODO Auto-generated method stub
+	public void run() {
+		init();
+		long lastLoopTime = System.nanoTime();
+		final int TARGET_FPS = 60;
+		final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
+		double delta =0;
+		
+		while(running){
+			long now = System.nanoTime();
+		    delta+=(now-lastLoopTime)/OPTIMAL_TIME;
+		    lastLoopTime=now;
+		    if(delta>=1){
+		    	tick();
+		    	delta--;
+		    }
+		    render();
+		    
+		    
+		    
+		}
+		stop();
+		
+	}
+	
+	private void init(){
+		try {
+			background = ImageIO.read(getClass().getResource("/images/play_background.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void tick(){
+		
+	}
+	
+	public void render(){
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs==null){
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		////////////////
+		
+		g.drawImage(background, 0, 0, this);
+		
+		/////////////////
+		
+		g.dispose();
+		
+		bs.show();
+		
+		
 		
 	}
 
@@ -37,8 +100,10 @@ public class Game extends Canvas implements Runnable{
 		game.setMaximumSize(new Dimension(WIDTH,HEIGHT));//setzt die maximale Größe des Spielfensters.
 		
 		frame.add(game);//Das Spiel wird dem JFrame hinzugefügt um Fensterpreferenzen zu übernehmen.
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Ermöglicht das Beenden des Programmes durch Schließen des Fensters.
 		frame.setUndecorated(true);//Entfernt die Menüleiste oben am Bildschirm (Vollbild)
+		frame.pack();
 		frame.setResizable(false);//Fenster kann nicht vergrößert oder verkleinert werden, für korrekt Skalierung notwendig.
 		frame.setLocationRelativeTo(null);//Bewegt Fesnter in die Mitte des Bildschirms.
 		frame.setVisible(true);//Macht das Fenster sichtbar.
