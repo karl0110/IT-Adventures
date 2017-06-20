c7da09bc14edd40c9a022e069e429fcfe2916f5b
Filip <Filip@192.168.178.37>
2017-06-20 09:51:58 +0200

    Lava Blokc Hinzugefügt und Farbe für Lava wegen Grafiken festgelegt Auß

--- IT-Adventures/src/com/it/main/LevelLoader.java
+++ IT-Adventures/src/com/it/main/LevelLoader.java
@@ -48,7 +48,7 @@ public class LevelLoader {
 				if(red==0 && green==0 && blue==0)handler.addObject(new Block(xx*32, yy*32, imageLoader, TileType.Dirt,dirt,player));
 				else if(red==0&&green==255&&blue==0)handler.addObject(new Block(xx*32,yy*32,imageLoader,TileType.Grass,gras,player));
 				else if(red==99&&green==99&&blue==99)handler.addObject(new Block(xx*32,yy*32,imageLoader,TileType.Stone,stone,player));
-				else if(red==99&&green==99&&blue==99)handler.addObject(new Block(xx*32,yy*32,imageLoader,TileType.Lava,lava,player));
+				else if(red==255&&green==255&&blue==0)handler.addObject(new Block(xx*32,yy*32,imageLoader,TileType.Lava,lava,player));
 				else if(red==0&&green==0&&blue==255)handler.addObject(new Enemy(xx*32,yy*32,imageLoader,TileType.Enemy,handler,EnemyType.Virus,player));
 			}
 		}
