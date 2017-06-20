ad93fafdf7f3508c316029c74847ca8d692d8919
Filip <Filip@DESKTOP-RKJ2HHJ.fritz.box>
2017-06-20 10:42:41 +0200

    Blöcke Hinzugefügt in TileType und farben bestimmt

--- IT-Adventures/src/com/it/main/TileType.java
+++ IT-Adventures/src/com/it/main/TileType.java
@@ -10,7 +10,12 @@ public enum TileType {
 	Dirt("/images/block_sprite_sheet.png",false,false,32,32,1,1),
 	Grass("/images/block_sprite_sheet.png",false,false,32,32,2,1),
 	Lava("/images/block_sprite_sheet.png",true,false,32,32,5,1),
+	Eis("/images/block_sprite_sheet.png",false,false,32,32,8,1),
+	EisUnten("/images/block_sprite_sheet.png",false,false,32,32,6,1),
+	EisOben("/images/block_sprite_sheet.png",false,false,32,32,7,1),
+	Burg("/images/block_sprite_sheet.png",false,false,32,32,9,1),
 	HangingGrass("/images/block_sprite_sheet.png",false,true,32,32,4,1);
+	
 	
 	
 	String imagePath;
